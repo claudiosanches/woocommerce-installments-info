fc42ad0347240a67b37566e9072fafb26066ae17
Claudio Sanches <contato@claudiosmweb.com>
2012-12-15 23:45:35 -0200

    fixed standards

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,11 @@
 <?php
 
 // If uninstall not called from WordPress exit.
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
-    exit ();
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) || !WP_UNINSTALL_PLUGIN || dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) ) ) {
+
+    status_header( 404 );
+    exit;
+}
 
 delete_option( 'wcccit_settings' );
 delete_option( 'wcccit_design' );
