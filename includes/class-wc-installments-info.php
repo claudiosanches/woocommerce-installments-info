418259269f6f2d0b94d88afac572aad084adcb86
claudiosmweb <contato@claudiosmweb.com>
2013-06-29 04:22:02 -0300

    fixed the callback_checkbox_cards method and admin_scripts method in WC_Installments_Info_Admin

--- includes/class-wc-installments-info.php
+++ includes/class-wc-installments-info.php
@@ -11,22 +11,11 @@ class WC_Installments_Info {
      * Construct.
      */
     public function __construct() {
+        $this->settings = get_option( 'wcii_settings' );
+        $this->design = get_option( 'wcii_design' );
 
-        // Add view in WooCommerce products single.
-        $views = get_option( 'wcii_design' );
-
-        if ( $views['display'] == 0 ) {
-            add_action( 'woocommerce_after_single_product_summary', array( &$this, 'print_view' ), 50 );
-
-        } else if ( $views['display'] == 1 ) {
-            add_action( 'woocommerce_after_single_product_summary', array( &$this, 'print_view' ), 1 );
-
-        } else if ( $views['display'] == 2 ) {
-            add_action( 'woocommerce_product_tabs', array( &$this, 'tab_view' ), 60 );
-            add_action( 'woocommerce_product_tab_panels', array( &$this, 'display_tab_view' ), 60 );
-        } else if ( $views['display'] == 3 ) {
-            add_action( 'woocommerce_single_product_summary', array( &$this, 'print_view' ), 35 );
-        }
+        // Displays the table.
+        $this->table_display();
 
         // Add Shortcode.
         add_shortcode( 'wcii', array( &$this, 'shortcode' ) );
@@ -40,9 +29,27 @@ class WC_Installments_Info {
      * Front-End Scripts.
      */
     public function front_scripts() {
-        if ( is_product() ) {
-            wp_register_style( 'wcii', plugins_url( 'css/styles.css', __FILE__ ), array(), null, 'all' );
-            wp_enqueue_style( 'wcii' );
+        if ( is_product() )
+            wp_enqueue_style( 'wcii-styles', WC_INSTALLMENTS_INFO_URL . 'assets/css/styles.css', array(), null, 'all' );
+    }
+
+    /**
+     * Displays the table in the products single.
+     *
+     * @return void
+     */
+    protected function table_display() {
+        if ( 0 == $this->design['display'] ) {
+            add_action( 'woocommerce_after_single_product_summary', array( &$this, 'print_view' ), 50 );
+
+        } else if ( 1 == $this->design['display'] ) {
+            add_action( 'woocommerce_after_single_product_summary', array( &$this, 'print_view' ), 1 );
+
+        } else if ( 2 == $this->design['display'] ) {
+            add_action( 'woocommerce_product_tabs', array( &$this, 'tab_view' ), 60 );
+            add_action( 'woocommerce_product_tab_panels', array( &$this, 'display_tab_view' ), 60 );
+        } else if ( 3 == $this->design['display'] ) {
+            add_action( 'woocommerce_single_product_summary', array( &$this, 'print_view' ), 35 );
         }
     }
 
@@ -61,174 +68,156 @@ class WC_Installments_Info {
 
         $price = number_format( $price, $num_decimals, stripslashes( get_option( 'woocommerce_price_decimal_sep' ) ), stripslashes( get_option( 'woocommerce_price_thousand_sep' ) ) );
 
-        if ( get_option( 'woocommerce_price_trim_zeros' ) == 'yes' && $num_decimals > 0 ) {
+        if ( 'yes' == get_option( 'woocommerce_price_trim_zeros' ) && 0 < $num_decimals )
             $price = woocommerce_trim_zeros( $price );
-        }
 
         return $price;
     }
 
     /**
-     * Format price.
+     * Installment calculator.
      *
-     * @param  string $price Product price.
-     * @return string        Price formatted.
+     * @param  float  $price               Product price.
+     * @param  int    $installment_maximum Quantity of installments.
+     * @param  int    $installment_minimum Installments minimum.
+     * @param  mixed  $iota                iota.
+     * @param  int    $without_interest    Installments without interest.
+     * @param  float  $interest            Interest.
+     * @param  int    $calculation_type    Calculation type (0 - Amortization schedule or 1 - Simple interest);
+     *
+     * @return array                       The installments.
      */
-    public function format_price( $price ) {
-        global $woocommerce;
+    protected function installment_calculator( $price, $installment_maximum, $installment_minimum, $iota, $without_interest, $interest, $calculation_type ) {
 
-        $return = '';
-        $currency_pos = get_option( 'woocommerce_currency_pos' );
-        $currency_symbol = get_woocommerce_currency_symbol();
+        $calcule = array();
 
-        $price = $this->number_format( $price );
+        if ( $price > $iota ) {
+            for ( $i = $installment_minimum; $i <= $installment_maximum; $i++ ) {
 
-        switch ( $currency_pos ) {
-            case 'left' :
-                $return = $currency_symbol . $price;
-            break;
-            case 'right' :
-                $return = $price . $currency_symbol;
-            break;
-            case 'left_space' :
-                $return = $currency_symbol . '&nbsp;' . $price;
-            break;
-            case 'right_space' :
-                $return = $price . '&nbsp;' . $currency_symbol;
-            break;
+                // Without interest.
+                if ( $i <= $without_interest )
+                    $installment_value = $price / $i;
+
+                // With interest.
+                if ( $i > $without_interest ) {
+                    if ( 0 == $calculation_type )
+                        $installment_value = ( $price * ( $interest / 100 ) ) / ( 1 - ( 1 / ( pow( 1 + ( $interest / 100 ), $i ) ) ) );
+
+                    if ( 1 == $calculation_type )
+                        $installment_value = ( $price * pow( 1 + ( $interest / 100 ), $i ) ) / $i;
+                }
+
+                // Test iota.
+                if ( $installment_value >= $iota )
+                    $calcule[] = woocommerce_price( $installment_value );
+                else
+                    break;
+            }
         }
 
-        return $return;
+        return $calcule;
     }
 
     /**
-     * Installment configurations.
+     * Installment view.
      *
-     * @param  float  $price            Product price.
-     * @param  int    $parcel_maximum   Quantity of parcels.
-     * @param  int    $parcel_minimum   Parcels minimum.
-     * @param  mixed  $iota             iota.
-     * @param  int    $without_interest Parcels without interest.
-     * @param  float  $interest         Interest.
-     * @param  int    $calculation_type Calculation type (0 - Amortization schedule or 1 - Simple interest);
-     * @return string                   Table with parcels and interest.
+     * @param  float  $price               Product price.
+     * @param  int    $installment_maximum Quantity of installments.
+     * @param  int    $installment_minimum Installments minimum.
+     * @param  mixed  $iota                iota.
+     * @param  int    $without_interest    Installments without interest.
+     * @param  float  $interest            Interest.
+     * @param  int    $calculation_type    Calculation type (0 - Amortization schedule or 1 - Simple interest);
+     *
+     * @return string                      Table with installments and interest.
      */
-    public function view(
+    protected function view(
         $price,
-        $parcel_maximum,
-        $parcel_minimum,
+        $installment_maximum,
+        $installment_minimum,
         $iota,
         $without_interest,
         $interest,
         $calculation_type ) {
 
-        // Get design options.
-        $design = get_option( 'wcii_design' );
-
-        // Get icons
-        $icons = get_option( 'wcii_icons', '' );
-
-        // Float ou margin.
-        $align = ( $design['float'] == 'center' ) ? ' margin-left: auto; margin-right: auto' : ' float: ' . $design['float'];
-
         $table = '';
 
-        if ( $price > $iota ) {
+        if ( ! empty( $price ) ) {
+            $values = $this->installment_calculator( $price, $installment_maximum, $installment_minimum, $iota, $without_interest, $interest, $calculation_type );
 
-            // Open the table.
-            $table .= '<div id="wc-credit-cart-table" class="clear" style="width: ' . $design['width'] . ';' . $align . ';">';
+            if ( ! empty( $values ) ) {
 
-            if ( $design['display'] != 2 ) {
-                $table .= '<h3>' . $design['title'] . '</h3>';
-            }
+                // Float ou margin.
+                $align = ( $this->design['float'] == 'center' ) ? ' margin-left: auto; margin-right: auto' : ' float: ' . $this->design['float'];
 
-            // Border wrapper.
-            $table .= '<div id="wc-credit-cart-table-wrap" style="border-color:' . $design['border'] . ';">';
+                $middle = ceil( count( $values ) / 2 );
 
-            $table .= '<div class="part left">';
+                // Open the table.
+                $table .= '<div id="wc-credit-cart-table" class="clear" style="width: ' . $this->design['width'] . ';' . $align . ';">';
 
-            $count = 0;
-            for ( $p = $parcel_minimum; $p <= $parcel_maximum; $p++ ) {
+                if ( 2 != $this->design['display'] )
+                    $table .= '<h3>' . $this->design['title'] . '</h3>';
 
-                $background = ( $count %  2 ) ? $design['even'] : $design['odd'];
+                // Border wrapper.
+                $table .= '<div id="wc-credit-cart-table-wrap" style="border-color:' . $this->design['border'] . ';">';
 
-                // Without interest.
-                if ( $p <= $without_interest ) {
-                    $parcel_value = $price / $p;
+                $table .= '<div class="part left">';
+
+                // Displays the installments.
+                foreach ( $values as $key => $value ) {
+                    $background = ( $key % 2 ) ? $this->design['even'] : $this->design['odd'];
+
+                    $key++;
+
+                    // Tests if has interest.
+                    if ( $key <= $without_interest )
+                        $table .= '<span class="card-info" style="color: ' . $this->design['without'] . '; background: ' . $background . '; border-color: ' . $this->design['border'] . ';">' . sprintf( __( '%s%sx%s of %s %swithout interest%s', 'wcii' ), '<strong>', $key, '</strong>', $value, '<em>', '</em>' ) . '</span>';
+                    else
+                        $table .= '<span class="card-info" style="background: ' . $background . '; border-color: ' . $this->design['border'] . ';">' . sprintf( __( '%s%sx%s of %s %swith interest%s', 'wcii' ), '<strong>', $key, '</strong>', $value, '<em>', '</em>' ) . '</span>';
+
+                    // Divides the table.
+                    if ( $key == $middle )
+                        $table .= '</div><div class="part right">';
                 }
 
-                // With interest.
-                if ( $p > $without_interest ) {
+                $table .= '</div>';
 
-                    if ( $calculation_type == 0 ) {
-                        $parcel_value = ( $price * ( $interest / 100 ) ) / ( 1 - ( 1 / ( pow( 1 + ( $interest / 100 ), $p ) ) ) );
-                    }
-
-                    if ( $calculation_type == 1 ) {
-                        $parcel_value = ( $price * pow( 1 + ( $interest / 100 ), $p ) ) / $p;
-                    }
-
-                }
-
-                // Test iota.
-                if ( $parcel_value >= $iota ) {
-
-                    if ( $p <= $without_interest ) {
-                        $table .= '<span class="card-info" style="color: ' . $design['without'] . '; background: ' . $background . '; border-color: ' . $design['border'] . ';">' . sprintf( __( '%s%sx%s of %s %swithout interest%s', 'wcii' ), '<strong>', $p, '</strong>', $this->format_price( $parcel_value ), '<em>', '</em>' ) . '</span>';
-                    } else {
-                        $table .= '<span class="card-info" style="background: ' . $background . '; border-color: ' . $design['border'] . ';">' . sprintf( __( '%s%sx%s of %s %swith interest%s', 'wcii' ), '<strong>', $p, '</strong>' , $this->format_price( $parcel_value ), '<em>', '</em>' ) . '</span>';
-                    }
-
-                }
-
-                if ( $p == intval( $parcel_maximum / 2 ) ) {
-                    $table .= '</div>';
-
-                    $table .= '<div class="part right">';
-                }
-
-                $count++;
-            }
-
-            $table .= '</div>';
-
-            // Close the border wrapper.
-            $table .= '<div class="clear"></div>';
-            $table .= '</div>';
-
-            // Details.
-            $table .= '<div id="wc-credit-cart-table-details">';
-
-            // Show interest info.
-            if ( $without_interest < $parcel_maximum ) {
-                $table .= '<span>' . sprintf( __( 'Interest of %s%s per month', 'wcii' ), $this->number_format( $interest ), '%' ) . '</span>';
-            }
-
-            // Show maximum parcel info.
-            if ( $iota > 0 ) {
-                $table .= '<span>' . sprintf( __( ' (parcel minimum of %s)', 'wcii' ), $this->format_price( $iota ) ) . '</span>';
-            }
-
-            // Close the details.
-            $table .= '</div>';
-
-            // Display credit card icons.
-            if ( $icons != '' ) {
-
-                $table .= '<div id="wc-credit-cart-table-icons">';
-                $table .= '<strong>' . __( 'Pay with: ', 'wcii' ) . '</strong>';
-
-                foreach ( $icons as $key => $value ) {
-                    $table .= sprintf( '<span class="card-icons card-%s"></span>', $value );
-                }
-
+                // Close the border wrapper.
                 $table .= '<div class="clear"></div>';
                 $table .= '</div>';
+
+                // Details.
+                $table .= '<div id="wc-credit-cart-table-details">';
+
+                // Show interest info.
+                if ( $without_interest < $installment_maximum )
+                    $table .= '<span>' . sprintf( __( 'Interest of %s%s per month', 'wcii' ), $this->number_format( $interest ), '%' ) . '</span>';
+
+                // Show maximum installment info.
+                if ( $iota > 0 )
+                    $table .= '<span>' . sprintf( __( ' (installment minimum of %s)', 'wcii' ), woocommerce_price( $iota ) ) . '</span>';
+
+                // Close the details.
+                $table .= '</div>';
+
+                // Close the table.
+                $table .= '</div>';
+
+                // Display credit card icons.
+                // if ( $icons != '' ) {
+
+                //     $table .= '<div id="wc-credit-cart-table-icons">';
+                //     $table .= '<strong>' . __( 'Pay with: ', 'wcii' ) . '</strong>';
+
+                //     foreach ( $icons as $key => $value ) {
+                //         $table .= sprintf( '<span class="card-icons card-%s"></span>', $value );
+                //     }
+
+                //     $table .= '<div class="clear"></div>';
+                //     $table .= '</div>';
+                // }
+
             }
-
-
-            // Close the table.
-            $table .= '</div>';
         }
 
         return $table;
@@ -240,17 +229,14 @@ class WC_Installments_Info {
     public function print_view() {
         global $product;
 
-        // Get settings.
-        $default = get_option( 'wcii_settings' );
-
-        echo $this->view( $product->get_price(), $default['parcel_maximum'], $default['parcel_minimum'], $default['iota'], $default['without_interest'], $default['interest'], $default['calculation_type'] );
+        echo $this->view( $product->get_price(), $this->settings['installment_maximum'], $this->settings['installment_minimum'], $this->settings['iota'], $this->settings['without_interest'], $this->settings['interest'], $this->settings['calculation_type'] );
     }
 
     /**
      * Create a new tab.
      */
     public function tab_view() {
-        echo '<li class="wcii_tab"><a href="#tab-wcii">' . apply_filters( 'wcii_tab_title' , __( 'Credit Card Parcels', 'wcii' ) ) . '</a></li>';
+        echo '<li class="wcii_tab"><a href="#tab-wcii">' . apply_filters( 'installments_info_tab_title' , __( 'Credit Card Installments', 'wcii' ) ) . '</a></li>';
     }
 
     /**
@@ -259,14 +245,9 @@ class WC_Installments_Info {
     public function display_tab_view() {
         global $product;
 
-        // Get settings.
-        $default = get_option( 'wcii_settings' );
-
-        $html = '<div class="panel entry-content" id="tab-wcii">';
-            $html .= $this->view( $product->price, $default['parcel_maximum'], $default['parcel_minimum'], $default['iota'], $default['without_interest'], $default['interest'], $default['calculation_type'] );
-        $html .= '</div>';
-
-        echo $html;
+        echo '<div class="panel entry-content" id="tab-wcii">';
+            $this->print_view();
+        echo '</div>';
     }
 
     /**
@@ -275,24 +256,21 @@ class WC_Installments_Info {
     public function shortcode( $atts ) {
         global $product;
 
-        // Get default settings.
-        $default = get_option( 'wcii_settings' );
-
         extract(
             shortcode_atts(
                 array(
-                    'price'            => $product->price,
-                    'parcel_maximum'   => $default['parcel_maximum'],
-                    'parcel_minimum'   => $default['parcel_minimum'],
-                    'iota'             => $default['iota'],
-                    'without_interest' => $default['without_interest'],
-                    'interest'         => $default['interest'],
-                    'calculation_type' => $default['calculation_type']
+                    'price'               => $product->price,
+                    'installment_maximum' => $this->settings['installment_maximum'],
+                    'installment_minimum' => $this->settings['installment_minimum'],
+                    'iota'                => $this->settings['iota'],
+                    'without_interest'    => $this->settings['without_interest'],
+                    'interest'            => $this->settings['interest'],
+                    'calculation_type'    => $this->settings['calculation_type']
                 ), $atts
             )
         );
 
-        return $this->view( $price, $parcel_maximum, $parcel_minimum, $iota, $without_interest, $interest, $calculation_type );
+        return $this->view( $price, $installment_maximum, $installment_minimum, $iota, $without_interest, $interest, $calculation_type );
     }
 
     /**
@@ -300,6 +278,7 @@ class WC_Installments_Info {
      */
     public function shortcode_register_buttons( $buttons ) {
         array_push( $buttons, '|', 'wcii' );
+
         return $buttons;
     }
 
@@ -308,6 +287,7 @@ class WC_Installments_Info {
      */
     public function shortcode_add_buttons( $plugin_array ) {
         $plugin_array['wcii'] = plugins_url( 'tinymce/wcii.js' , __FILE__ );
+
         return $plugin_array;
     }
 
@@ -315,11 +295,10 @@ class WC_Installments_Info {
      * Register buttons in init.
      */
     public function shortcode_buttons_init() {
-        if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) ) {
+        if ( ! current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) )
             return;
-        }
 
-        if ( get_user_option( 'rich_editing') == 'true' ) {
+        if ( 'true' == get_user_option( 'rich_editing') ) {
             add_filter( 'mce_external_plugins', array( &$this, 'shortcode_add_buttons' ) );
             add_filter( 'mce_buttons', array( &$this, 'shortcode_register_buttons' ) );
         }
