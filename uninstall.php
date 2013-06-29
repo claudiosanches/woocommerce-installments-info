451ca0bbd41da182b666cfc20a8a20d539783fb9
claudiosmweb <contato@claudiosmweb.com>
2013-06-29 03:01:33 -0300

    updated the uninstall file

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,11 @@
 <?php
 
 // If uninstall not called from WordPress exit.
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) || !WP_UNINSTALL_PLUGIN || dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) ) ) {
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) || ! WP_UNINSTALL_PLUGIN || dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) ) ) {
 
     status_header( 404 );
     exit;
 }
 
-delete_option( 'wcccit_settings' );
-delete_option( 'wcccit_design' );
-delete_option( 'wcccit_icons' );
+delete_option( 'wcii_settings' );
+delete_option( 'wcii_design' );
